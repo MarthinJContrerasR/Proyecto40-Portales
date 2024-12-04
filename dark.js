d71f1a8f24293685b66ca28d0c70b83ac10ee2f1
MarthinJContrerasR <marj_contrerasr@unicah.edu>
2024-12-03 23:07:19 -0600

    Archivo JS agregando modo Dark

---
+++ dark.js
@@ -0,0 +1,39 @@
+const btnDark = document.querySelector("#btn-dark");
+const body = document.querySelector("body");
+const tarjeta = document.querySelector(".darkBTN");
+
+const estadoDarkMode = localStorage.getItem("dark-mode");
+
+function activarModoDark(){
+    body.classList.add("modo-dark__body");
+    tarjeta.classList.add("modo-dark__tarjeta");
+    localStorage.setItem("dark-mode","activado");
+}
+
+function desactivarModoDark(){
+    body.classList.remove("modo-dark__body");
+    tarjeta.classList.remove("modo-dark__tarjeta");
+    localStorage.setItem("dark-mode","desactivado");
+}
+
+if(estadoDarkMode === "activado"){
+    activarModoDark();
+    btnDark.textContent="Cambiar a modo light";
+}
+else{
+    desactivarModoDark();
+    btnDark.textContent="Cambiar a modo Dark";
+}
+
+btnDark.addEventListener("click",()=>{
+    const estadoDarkMode = localStorage.getItem("dark-mode");
+    
+    if(estadoDarkMode==="desactivado"){
+        activarModoDark();
+        btnDark.textContent="Cambiar a modo light";
+    }
+    else{
+        desactivarModoDark();
+        btnDark.textContent="Cambiar a modo dark";
+    }
+});
